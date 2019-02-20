00f52979c75eb69b5de78e25c4881e9ff3833e85
Joe Soultanis <jsoultanis@uber.com>
2019-02-20 15:43:11 -0800

    adding TODO for writeWithoutResponse in GattIO

--- Source/core/CoreGattIO.swift
+++ Source/core/CoreGattIO.swift
@@ -122,6 +122,8 @@ class CoreGattIO: NSObject, GattIO, CBPeripheralDelegate {
     
     public func write(service: CBUUID, characteristic: CBUUID, data: Data) -> Completable {
         
+        
+        // TODO: don't wait for didWriteToCharacteristicSubject when performing a .writeWithoutResponse to avoid Rx timeout
         let sharedWriteCompletable: Completable =
             connectionState
                 .flatMapLatest { (state: ConnectionManagerState) -> Observable<([CBService], Error?)> in
