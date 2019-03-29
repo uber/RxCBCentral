856b8f6620d0f5ad15c04978eca170b89c19e74a
Joe Soultanis <jsoultanis@uber.com>
2019-03-29 10:52:14 -0700

    refactor to ignore RSSI for distinctUntilChanged

--- Source/core/Helper/RSSIScanMatcher.swift
+++ Source/core/Helper/RSSIScanMatcher.swift
@@ -6,34 +6,35 @@ import CoreBluetooth
 import RxSwift
 
 // A ScanMatcher that matches us with the peripheral that is closest in proximity
-class RSSIScanMatcher: ScanMatching {
+public final class RSSIScanMatcher: ScanMatching {
     
     // If we haven't discovered any new peripherals in `discoverInterval` seconds, return best matching peripheral
     static let defaultDiscoverInterval: RxTimeInterval = 5
     
-    init() {}
+    public init(scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: .default)) {
+        self.scheduler = scheduler
+    }
     
-    func accept(_ scanData: ScanData) -> Observable<CBPeripheral> {
+    public func accept(_ scanData: ScanData) -> Observable<CBPeripheral> {
         // add newly discovered peripheral to dictionary
-        peripherals[scanData.peripheral] = scanData.RSSI.doubleValue
-        peripheralsSubject.onNext(peripherals)
+        peripheralsDict[scanData.peripheral] = scanData.RSSI.doubleValue
+        peripheralsSubject.onNext(scanData.peripheral)
         
         return
             peripheralsSubject
                 .asObservable()
                 // TODO: test this in test harnass once complete
-                // if Apple's didDiscoverPeripheral func re-emits the same peripherals with new RSSIs over time,
-                // we need to change this distinctUntilChanged to only check CBPeripherals and not their RSSI values
+                // we ignore peripherals that may be rediscovered with changing RSSIs for now, and only take the first calculated RSSI
                 .distinctUntilChanged()
                 .flatMapLatest { _ -> Observable<Int> in
                     // resets the timer every time new peripherals are discovered
-                    return Observable<Int>.timer(RSSIScanMatcher.defaultDiscoverInterval, scheduler: ConcurrentDispatchQueueScheduler(qos: .default))
+                    return Observable<Int>.timer(RSSIScanMatcher.defaultDiscoverInterval, scheduler: self.scheduler)
                 }
                 .take(1)
                 .flatMapLatest { _ -> Observable<CBPeripheral> in
                     // after no new peripherals are discovered for a few seconds, return the closest one
                     return self.getMinRSSIPeripheral()
-        }
+                }
     }
     
     // A helper function that returns the peripheral that is closest in proximity, if one exists.
@@ -41,7 +42,7 @@ class RSSIScanMatcher: ScanMatching {
         return
             Observable.create { observer in
                 // note: the closer RSSI is to 0, the closer in proximity a device is. That is why we use absolute value in our min calculation.
-                let minRSSIPeripheralData = self.peripherals.min { pair1, pair2 in abs(pair1.value) < abs(pair2.value) }
+                let minRSSIPeripheralData = self.peripheralsDict.min { pair1, pair2 in abs(pair1.value) < abs(pair2.value) }
                 
                 if let minRSSIPeripheralData = minRSSIPeripheralData {
                     observer.onNext(minRSSIPeripheralData.key)
@@ -49,11 +50,12 @@ class RSSIScanMatcher: ScanMatching {
                 }
                 
                 return Disposables.create()
-        }
+            }
     }
     
     private typealias RSSI = Double
-    private var peripherals = [CBPeripheral: RSSI]()
-    private let peripheralsSubject = ReplaySubject<[CBPeripheral: RSSI]>.create(bufferSize: 1)
+    private var peripheralsDict = [CBPeripheral: RSSI]()
+    private let peripheralsSubject = ReplaySubject<CBPeripheral>.create(bufferSize: 1)
+    private let scheduler: SchedulerType
 }
 
